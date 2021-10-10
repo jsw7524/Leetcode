e18566625e29fd3066494830d626e41d95f4f16c
jsw7524 <jsw7524@gmail.com>
2021-10-10 18:17:53 +0800

    2033. Minimum Operations to Make a Uni-Value Grid

--- 5895. Minimum Operations to Make a Uni-Value Grid/5895. Minimum Operations to Make a Uni-Value Grid.py
+++ 5895. Minimum Operations to Make a Uni-Value Grid/5895. Minimum Operations to Make a Uni-Value Grid.py
@@ -2,45 +2,24 @@ from typing import List
 
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        flat_list = [item for sublist in grid for item in sublist]
-        avg=sum(flat_list)/len(flat_list)
-        minElement = min(flat_list)
-        maxElement = max(flat_list)
-        
-        minUniValue= x * ((avg-minElement)//x)+minElement
-        maxUniValue= maxElement - x * ((maxElement-avg)//x)
-        
+        flat_list = sorted([item for sublist in grid for item in sublist])
+        median=flat_list[len(flat_list)//2]
+        minOp=0
         for e in flat_list:
-            if abs(e-minUniValue) % x != 0:
+            if abs(e-median) % x !=0:
                 return -1
-        
-        minOp=2**31-1
-        counter=0
-        while True:
-            for e in flat_list:
-                counter += abs(e-minUniValue)//x
-            if minOp > counter:
-                minOp=counter
-                counter=0
-                minUniValue-=x
-            else:
-                break
-        counter=0        
-        while True:
-            for e in flat_list:
-                counter += abs(e-maxUniValue)//x
-            if minOp > counter:
-                minOp=counter
-                counter=0
-                maxUniValue+=x
-            else:
-                break                    
-        return int(minOp)
+            minOp+=abs(e-median)//x
+        return minOp
+
+def main():
+    sln=Solution()
+    assert 4==sln.minOperations(grid = [[2,4],[6,8]], x = 2)
+    assert 5==sln.minOperations(grid = [[1,5],[2,3]], x = 1)
+    assert -1==sln.minOperations(grid = [[1,2],[3,4]], x = 2)
+    assert 25==sln.minOperations(grid = [[529,529,989],[989,529,345],[989,805,69]], x = 92)
+    assert 12==sln.minOperations(grid = [[931,128],[639,712]], x = 73)    
                 
+if __name__ == '__main__':
+    main()
+
         
-sln=Solution()
-assert 4==sln.minOperations(grid = [[2,4],[6,8]], x = 2)
-assert 5==sln.minOperations(grid = [[1,5],[2,3]], x = 1)
-assert -1==sln.minOperations(grid = [[1,2],[3,4]], x = 2)
-assert 25==sln.minOperations(grid = [[529,529,989],[989,529,345],[989,805,69]], x = 92)
-assert 12==sln.minOperations(grid = [[931,128],[639,712]], x = 73)
