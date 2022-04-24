d0ce2dd20509554246ab588b0ac27ada987e73d7
jsw7524 <jsw7524@gmail.com>
2022-04-24 13:40:47 +0800

    6042. Count Lattice Points Inside a Circle Ver.2

--- 6042. Count Lattice Points Inside a Circle/6042. Count Lattice Points Inside a Circle.py
+++ 6042. Count Lattice Points Inside a Circle/6042. Count Lattice Points Inside a Circle.py
@@ -1,16 +1,17 @@
 from typing import List
 
 class Solution:
-    def countLatticePoints(self, circles: List[List[int]]) -> int:
-        grid = [[0 for x in range(201)] for y in range(201)]
-        
+    def countLatticePoints(self, circles: List[List[int]]) -> int:    
+        record={}
+        counter=0
         for x, y, r in circles:
             for i in range(x-r, x+r+1):
                 for j in range(y-r, y+r+1):
                     if (i-x)**2 + (j-y)**2 <= r**2:
-                        grid[i][j] = 1
-        
-        return sum(sum(row) for row in grid)
+                        if (i,j) not in record:
+                            counter += 1
+                            record[(i,j)]=1
+        return counter
     
 sln=Solution()
 assert 5==sln.countLatticePoints(circles = [[2,2,1]])
